1075651523eb964aa4e72216ad2168e4ec776f16
Higor Pereira <phigor637@gmail.com>
2025-04-10 22:25:34 -0300

    aula de components

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,32 @@
-import CustomButton from "@/components/Title";
+import CustomButton from "@/components/CustomButton";
 import React, { useState } from "react";
-import { Button, Text, View } from "react-native";
+import  { Button, Text, TextInput, View } from "react-native";
 
 export default function HomeScreen() {
-    const [count, setCount] = useState(0);
-
+ const name ="Higor";
+    
+    const [count, setCount] = useState(0); 
+    const [nome, setNome] = useState(name);
     
 
     return(
-        <View>
-            <Text>Bem-vindo ao meu App</Text>
+        <View   style={{flex: 1, justifyContent:"center", alignItems:"center"}}>
+            <Text>Bem-vindo ao meu App  {nome}</Text>
             <Text>Contador: {count}</Text>
             <Button title="Aumentar" onPress={() => {setCount(count + 1)}}/>
             <Button title="Diminuir" onPress={() => {setCount(count - 1)}}/>
             <Button title="Resetar" onPress={() => {setCount(- 0)}}/>
-            <CustomButton title="NAME"/>
+            
+            <TextInput placeholder="Digite algo" onChangeText={setNome}/>
+            
+            <CustomButton 
+            title="botão azul"
+            onPress={()=> {setNome('Higor' + count)}}
+        
+        
+        />
         </View>
+
     );
 
 }
